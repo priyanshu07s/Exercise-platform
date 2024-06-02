ecfcf9ee13e979bc5d90d5df0f5243eb7c097b58
priyanshu07s <122457249+priyanshu07s@users.noreply.github.com>
2024-06-02 11:29:51 +0530

    exercise.py

--- exercise.py
+++ exercise.py
@@ -1,1 +1,58 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+
+# Initialize MediaPipe Pose
+mp_pose = mp.solutions.pose
+pose = mp_pose.Pose()
+
+# Initialize webcam
+cap = cv2.VideoCapture(0)
+
+
+def calculate_angle(a, b, c):
+    a = np.array(a)  # First point
+    b = np.array(b)  # Mid point
+    c = np.array(c)  # End point
+
+    radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
+    angle = np.abs(radians * 180.0 / np.pi)
+
+    if angle > 180.0:
+        angle = 360 - angle
+
+    return angle
+
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = pose.process(image)
+
+    if results.pose_landmarks:
+        landmarks = results.pose_landmarks.landmark
+
+        shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
+                    landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+        elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x, landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+        wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x, landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+
+        angle = calculate_angle(shoulder, elbow, wrist)
+        print(angle)
+
+        # Display the angle on the frame
+        cv2.putText(frame, str(angle), tuple(np.multiply(elbow, [640, 480]).astype(int)),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA
+                    )
+
+        # Drawing landmarks
+        mp.solutions.drawing_utils.draw_landmarks(frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+
+    cv2.imshow('Pushup Pose Estimation', frame)
+
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
 
